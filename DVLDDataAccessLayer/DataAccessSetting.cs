fc52a14786c249d23987fbe109ab84525c35d163
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-10-09 15:07:19 +0300

    DataAccessSetting

--- DVLDDataAccessLayer/DataAccessSetting.cs
+++ DVLDDataAccessLayer/DataAccessSetting.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DVLDDataAccessLayer
 {
-    public class Class1
+    public class clsDataAccessSetting
     {
+        public static string ConnectionString = "Server=localhost\\SQL22;Database=My_DVLD;Trusted_Connection=True;";
     }
 }
