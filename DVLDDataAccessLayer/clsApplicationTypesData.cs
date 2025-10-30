e6238812dd14345ce516a2a65c30d419ad386433
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-10-30 21:13:33 +0300

    Added BL and DAL for ApplicationTypes

---
+++ DVLDDataAccessLayer/clsApplicationTypesData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDDataAccessLayer
+{
+    internal class clsApplicationsTypesData
+    {
+    }
+}
