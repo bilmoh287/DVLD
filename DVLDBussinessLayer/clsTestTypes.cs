b005ab6eb5200554a1fec1b9fa4dd1911b443a02
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-10-31 12:20:01 +0300

    Added BL and DAL for TestTypes

---
+++ DVLDBussinessLayer/clsTestTypes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using DVLDDataAccessLayer;
+
+namespace DVLDBussinessLayer
+{
+    public class clsTestTypes
+    {
+        public enum enMode { AddNewMode = 0, UpdateMode = 1 };
+        public enMode _Mode = enMode.AddNewMode;
+
+        public int TestTypeID { get; set; }
+        public string TestTypeTitle { get; set; }
+        public string TestTypeDescription { get; set; }
+        public decimal TestTypeFees { get; set; }
+
+        public clsTestTypes()
+        {
+            TestTypeID = -1;
+            TestTypeTitle = "";
+            TestTypeDescription = "";
+            TestTypeFees = 0;
+
+            _Mode = enMode.AddNewMode;
+        }
+
+        private clsTestTypes(int testTypeID, string title, string description, decimal fees)
+        {
+            TestTypeID = testTypeID;
+            TestTypeTitle = title;
+            TestTypeDescription = description;
+            TestTypeFees = fees;
+
+            _Mode = enMode.UpdateMode;
+        }
+
+        public static DataTable GetAllTestTypesList()
+        {
+            return clsTestTypesData.GetAllTestTypesList();
+        }
+
+        public static clsTestTypes Find(int testTypeID)
+        {
+            string title = "";
+            string description = "";
+            decimal fees = 0;
+
+            if (clsTestTypesData.GetTestTypeByID(testTypeID, ref title, ref description, ref fees))
+            {
+                return new clsTestTypes(testTypeID, title, description, fees);
+            }
+
+            return null;
+        }
+
+        private bool _AddNewTestType()
+        {
+            this.TestTypeID = clsTestTypesData.AddNewTestType(this.TestTypeTitle, this.TestTypeDescription, this.TestTypeFees);
+            return (this.TestTypeID != -1);
+        }
+
+        private bool _UpdateTestType()
+        {
+            return clsTestTypesData.UpdateTestType(this.TestTypeID, this.TestTypeTitle, this.TestTypeDescription, this.TestTypeFees);
+        }
+
+        public static bool Delete(int testTypeID)
+        {
+            return clsTestTypesData.DeleteTestType(testTypeID);
+        }
+
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+                case enMode.AddNewMode:
+                    if (_AddNewTestType())
+                    {
+                        _Mode = enMode.UpdateMode;
+                        return true;
+                    }
+                    else
+                        return false;
+
+                case enMode.UpdateMode:
+                    return _UpdateTestType();
+            }
+
+            return false;
+        }
+    }
+}
---
+++ DVLDDataAccessLayer/clsTestTypesData.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDDataAccessLayer
+{
+    public class clsTestTypesData
+    {
+        // 🟢 GET ALL TEST TYPES
+        public static DataTable GetAllTestTypesList()
+        {
+            DataTable dtTestTypes = new DataTable();
+
+            string query = "SELECT * FROM TestTypes;";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        dtTestTypes.Load(reader);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error loading TestTypes: " + ex.Message);
+                }
+            }
+
+            return dtTestTypes;
+        }
+
+        // GET ONE TEST TYPE BY ID
+        public static bool GetTestTypeByID(
+            int TestTypeID,
+            ref string TestTypeTitle,
+            ref string TestTypeDescription,
+            ref decimal TestTypeFees)
+        {
+            bool isFound = false;
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            {
+                string query = @"SELECT * FROM TestTypes WHERE TestTypeID = @TestTypeID;";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            TestTypeTitle = reader["TestTypeTitle"].ToString();
+                            TestTypeDescription = reader["TestTypeDescription"].ToString();
+                            TestTypeFees = Convert.ToDecimal(reader["TestTypeFees"]);
+                            isFound = true;
+                        }
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error reading TestType: " + ex.Message);
+                    }
+                }
+            }
+
+            return isFound;
+        }
+
+        // ADD NEW TEST TYPE
+        public static int AddNewTestType(string TestTypeTitle, string TestTypeDescription, decimal TestTypeFees)
+        {
+            int newID = -1;
+
+            string query = @"
+                INSERT INTO TestTypes (TestTypeTitle, TestTypeDescription, TestTypeFees)
+                VALUES (@TestTypeTitle, @TestTypeDescription, @TestTypeFees);
+                SELECT SCOPE_IDENTITY();";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@TestTypeTitle", TestTypeTitle);
+                command.Parameters.AddWithValue("@TestTypeDescription", TestTypeDescription);
+                command.Parameters.AddWithValue("@TestTypeFees", TestTypeFees);
+
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+
+                    if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                    {
+                        newID = insertedID;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error adding TestType: " + ex.Message);
+                }
+            }
+
+            return newID;
+        }
+
+        // 🟠 UPDATE EXISTING TEST TYPE
+        public static bool UpdateTestType(int TestTypeID, string TestTypeTitle, string TestTypeDescription, decimal TestTypeFees)
+        {
+            bool isUpdated = false;
+
+            string query = @"
+                UPDATE TestTypes
+                SET 
+                    TestTypeTitle = @TestTypeTitle,
+                    TestTypeDescription = @TestTypeDescription,
+                    TestTypeFees = @TestTypeFees
+                WHERE TestTypeID = @TestTypeID;";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+                command.Parameters.AddWithValue("@TestTypeTitle", TestTypeTitle);
+                command.Parameters.AddWithValue("@TestTypeDescription", TestTypeDescription);
+                command.Parameters.AddWithValue("@TestTypeFees", TestTypeFees);
+
+                try
+                {
+                    connection.Open();
+                    int rows = command.ExecuteNonQuery();
+                    isUpdated = rows > 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error updating TestType: " + ex.Message);
+                }
+            }
+
+            return isUpdated;
+        }
+
+        // 🔴 DELETE TEST TYPE
+        public static bool DeleteTestType(int TestTypeID)
+        {
+            bool isDeleted = false;
+
+            string query = "DELETE FROM TestTypes WHERE TestTypeID = @TestTypeID;";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+                try
+                {
+                    connection.Open();
+                    int rows = command.ExecuteNonQuery();
+                    isDeleted = rows > 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error deleting TestType: " + ex.Message);
+                }
+            }
+
+            return isDeleted;
+        }
+    }
+}
