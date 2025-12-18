0d29717030b81093fafcca71c4bae33b616d513d
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-12-18 10:17:41 +0300

    Replacement for Lost or Damaged UI Design

---
+++ DVLDPresentationLayer/Applications/Replace Lost or Damaged License/frmReplaceLostOrDamagedLicense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLDPresentationLayer.Applications.Replace_Lost_or_Damaged_License
+{
+    public partial class frmReplaceLostOrDamagedLicense : Form
+    {
+        public frmReplaceLostOrDamagedLicense()
+        {
+            InitializeComponent();
+        }
+    }
+}
