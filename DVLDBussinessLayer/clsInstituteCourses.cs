ac5040e981d46d9ba4bdd018422e96920e666244
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-04-05 16:58:58 +0300

    BL and DAL for Institute Course

---
+++ DVLDBussinessLayer/clsInstituteCourses.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVLDDataAccessLayer;
+
+namespace DVLDBussinessLayer
+{
+    public class clsInstituteCourses
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int CourseID { get; set; }
+        public int InstituteID { get; set; }
+        public string CourseName { get; set; }
+        public int DurationInDays { get; set; }
+        public decimal CourseFee { get; set; }
+
+        public clsInstituteCourses()
+        {
+            this.CourseID = -1;
+            this.InstituteID = -1;
+            this.CourseName = "";
+            this.DurationInDays = 0;
+            this.CourseFee = 0;
+            Mode = enMode.AddNew;
+        }
+
+        private clsInstituteCourses(int CourseID, int InstituteID, string CourseName, int DurationInDays, decimal CourseFee)
+        {
+            this.CourseID = CourseID;
+            this.InstituteID = InstituteID;
+            this.CourseName = CourseName;
+            this.DurationInDays = DurationInDays;
+            this.CourseFee = CourseFee;
+            Mode = enMode.Update;
+        }
+
+        public static clsInstituteCourses Find(int CourseID)
+        {
+            int InstituteID = -1, DurationInDays = 0;
+            string CourseName = "";
+            decimal CourseFee = 0;
+
+            if (clsInstituteCourseData.GetCourseInfoByID(CourseID, ref InstituteID, ref CourseName, ref DurationInDays, ref CourseFee))
+            {
+                return new clsInstituteCourses(CourseID, InstituteID, CourseName, DurationInDays, CourseFee);
+            }
+            return null;
+        }
+
+        private bool _AddNew()
+        {
+            this.CourseID = clsInstituteCourseData.AddNewCourse(this.InstituteID, this.CourseName, this.DurationInDays, this.CourseFee);
+            return (this.CourseID != -1);
+        }
+
+        private bool _Update()
+        {
+            return clsInstituteCourseData.UpdateCourse(this.CourseID, this.InstituteID, this.CourseName, this.DurationInDays, this.CourseFee);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNew())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+                case enMode.Update:
+                    return _Update();
+            }
+            return false;
+        }
+
+        public static DataTable GetCoursesList(int InstituteID)
+        {
+            return clsInstituteCourseData.GetAllCoursesByInstituteID(InstituteID);
+        }
+    }
+}
---
+++ DVLDDataAccessLayer/clsInstituteCourseData.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDDataAccessLayer
+{
+    public class clsInstituteCourseData
+    {
+        public static bool GetCourseInfoByID(int CourseID, ref int InstituteID,
+            ref string CourseName, ref int DurationInDays, ref decimal CourseFee)
+        {
+            bool isFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+                {
+                    string query = "SELECT * FROM InstituteCourses WHERE CourseID = @CourseID";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@CourseID", CourseID);
+
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        isFound = true;
+                        InstituteID = (int)reader["InstituteID"];
+                        CourseName = (string)reader["CourseName"];
+                        DurationInDays = (int)reader["DurationInDays"];
+                        CourseFee = (decimal)reader["CourseFee"];
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception) { isFound = false; }
+            return isFound;
+        }
+
+        public static int AddNewCourse(int InstituteID, string CourseName, int DurationInDays, decimal CourseFee)
+        {
+            int CourseID = -1;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+                {
+                    string query = @"INSERT INTO InstituteCourses (InstituteID, CourseName, DurationInDays, CourseFee)
+                                     VALUES (@InstituteID, @CourseName, @DurationInDays, @CourseFee);
+                                     SELECT SCOPE_IDENTITY();";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@InstituteID", InstituteID);
+                    command.Parameters.AddWithValue("@CourseName", CourseName);
+                    command.Parameters.AddWithValue("@DurationInDays", DurationInDays);
+                    command.Parameters.AddWithValue("@CourseFee", CourseFee);
+
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                    {
+                        CourseID = insertedID;
+                    }
+                }
+            }
+            catch (Exception) { }
+            return CourseID;
+        }
+
+        public static bool UpdateCourse(int CourseID, int InstituteID, string CourseName, int DurationInDays, decimal CourseFee)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+                {
+                    string query = @"UPDATE InstituteCourses 
+                                     SET InstituteID = @InstituteID, CourseName = @CourseName, 
+                                         DurationInDays = @DurationInDays, CourseFee = @CourseFee 
+                                     WHERE CourseID = @CourseID";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@CourseID", CourseID);
+                    command.Parameters.AddWithValue("@InstituteID", InstituteID);
+                    command.Parameters.AddWithValue("@CourseName", CourseName);
+                    command.Parameters.AddWithValue("@DurationInDays", DurationInDays);
+                    command.Parameters.AddWithValue("@CourseFee", CourseFee);
+
+                    connection.Open();
+                    rowsAffected = command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception) { return false; }
+            return (rowsAffected > 0);
+        }
+
+        public static DataTable GetAllCoursesByInstituteID(int InstituteID)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+                {
+                    string query = "SELECT * FROM InstituteCourses WHERE InstituteID = @InstituteID";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@InstituteID", InstituteID);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows) dt.Load(reader);
+                    reader.Close();
+                }
+            }
+            catch (Exception) { }
+            return dt;
+        }
+    }
+}
