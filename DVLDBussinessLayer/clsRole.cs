0de76eeacf0f01cb047b494b289bf6fe868a79d4
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-04-13 18:16:43 +0300

    Added Role BL and DAL

---
+++ DVLDBussinessLayer/clsRole.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVLDDataAccessLayer;
+
+namespace DVLDBussinessLayer
+{
+    public class clsRole
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public int PermissionsMask { get; set; }
+        public string Description { get; set; }
+
+        public clsRole()
+        {
+            RoleID = -1;
+            RoleName = "";
+            PermissionsMask = 0;
+            Description = "";
+            Mode = enMode.AddNew;
+        }
+
+        private clsRole(int RoleID, string RoleName, int PermissionsMask, string Description)
+        {
+            this.RoleID = RoleID;
+            this.RoleName = RoleName;
+            this.PermissionsMask = PermissionsMask;
+            this.Description = Description;
+            Mode = enMode.Update;
+        }
+
+        public static clsRole Find(int RoleID)
+        {
+            string RoleName = "";
+            int PermissionsMask = 0;
+            string Description = "";
+
+            if (clsRoleData.GetRoleByID(RoleID, ref RoleName, ref PermissionsMask, ref Description))
+            {
+                return new clsRole(RoleID, RoleName, PermissionsMask, Description);
+            }
+            return null;
+        }
+
+        private bool _AddNew()
+        {
+            this.RoleID = clsRoleData.AddNewRole(RoleName, PermissionsMask, Description);
+            return (this.RoleID != -1);
+        }
+
+        private bool _Update()
+        {
+            return clsRoleData.UpdateRole(RoleID, RoleName, PermissionsMask, Description);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNew())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+
+                case enMode.Update:
+                    return _Update();
+            }
+            return false;
+        }
+
+        public static DataTable GetAllRoles()
+        {
+            return clsRoleData.GetAllRoles();
+        }
+
+        public static bool DeleteRole(int RoleID)
+        {
+            return clsRoleData.DeleteRole(RoleID);
+        }
+    }
+        
+}
---
+++ DVLDDataAccessLayer/clsRoleData.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDDataAccessLayer
+{
+    public class clsRoleData
+    {
+        public static DataTable GetAllRoles()
+        {
+            DataTable dt = new DataTable();
+            string query = @"SELECT RoleID, RoleName, PermissionsMask, Description FROM Roles";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    dt.Load(command.ExecuteReader());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error loading roles: " + ex.Message);
+                }
+            }
+            return dt;
+        }
+
+        public static bool GetRoleByID(int RoleID, ref string RoleName, ref int PermissionsMask, ref string Description)
+        {
+            bool isFound = false;
+            string query = @"SELECT * FROM Roles WHERE RoleID = @RoleID";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@RoleID", RoleID);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        isFound = true;
+                        RoleName = (string)reader["RoleName"];
+                        PermissionsMask = (int)reader["PermissionsMask"];
+                        Description = reader["Description"]?.ToString();
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error loading role: " + ex.Message);
+                }
+            }
+
+            return isFound;
+        }
+
+        public static int AddNewRole(string RoleName, int PermissionsMask, string Description)
+        {
+            int newID = -1;
+
+            string query = @"INSERT INTO Roles (RoleName, PermissionsMask, Description)
+                             VALUES (@RoleName, @PermissionsMask, @Description);
+                             SELECT SCOPE_IDENTITY();";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@RoleName", RoleName);
+                command.Parameters.AddWithValue("@PermissionsMask", PermissionsMask);
+                command.Parameters.AddWithValue("@Description", (object)Description ?? DBNull.Value);
+
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+
+                    if (result != null)
+                        newID = Convert.ToInt32(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error adding role: " + ex.Message);
+                }
+            }
+
+            return newID;
+        }
+
+        public static bool UpdateRole(int RoleID, string RoleName, int PermissionsMask, string Description)
+        {
+            int rowsAffected = 0;
+
+            string query = @"UPDATE Roles
+                             SET RoleName = @RoleName,
+                                 PermissionsMask = @PermissionsMask,
+                                 Description = @Description
+                             WHERE RoleID = @RoleID";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@RoleID", RoleID);
+                command.Parameters.AddWithValue("@RoleName", RoleName);
+                command.Parameters.AddWithValue("@PermissionsMask", PermissionsMask);
+                command.Parameters.AddWithValue("@Description", (object)Description ?? DBNull.Value);
+
+                try
+                {
+                    connection.Open();
+                    rowsAffected = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error updating role: " + ex.Message);
+                }
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static bool DeleteRole(int RoleID)
+        {
+            int rowsAffected = 0;
+
+            string query = @"DELETE FROM Roles WHERE RoleID = @RoleID";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@RoleID", RoleID);
+
+                try
+                {
+                    connection.Open();
+                    rowsAffected = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error deleting role: " + ex.Message);
+                }
+            }
+
+            return (rowsAffected > 0);
+        }
+    }
+}
