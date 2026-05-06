d5231d3ed7eada52c4acdc4185dc8eac12431c2a
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-05-06 16:33:00 +0300

    feat(dto): add application status response

--- DVLDDataAccessLayer/DTOs/ApplicationStatusResponseDTO.cs
+++ DVLDDataAccessLayer/DTOs/ApplicationStatusResponseDTO.cs
@@ -5,6 +5,7 @@ namespace DVLDDataAccessLayer.DTOs
     public class ApplicationStatusResponseDTO
     {
         public int ApplicationID { get; set; }
+        public int LDLApplicationID { get; set; }
         public string ClassName { get; set; }
         public string Status { get; set; }
         public DateTime AppliedDate { get; set; }
