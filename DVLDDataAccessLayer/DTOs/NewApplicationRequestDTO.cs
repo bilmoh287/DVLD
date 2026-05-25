57a396005d43b93ed685555dd2abbc8c9115497b
bilmoh287 <bilmoh287@gmail.com>
2026-05-25 07:25:26 -0800

    Error Corrected

--- DVLDDataAccessLayer/DTOs/NewApplicationRequestDTO.cs
+++ DVLDDataAccessLayer/DTOs/NewApplicationRequestDTO.cs
@@ -6,10 +6,10 @@ namespace DVLDDataAccessLayer.DTOs
         public int LicenseClassID { get; set; }
         public int DrivingInstituteID { get; set; }
         
-        public string? FrontIdBase64 { get; set; }
-        public string? BackIdBase64 { get; set; }
-        public string? BirthCertBase64 { get; set; }
-        public string? TranscriptBase64 { get; set; }
+        public string FrontIdBase64 { get; set; }
+        public string BackIdBase64 { get; set; }
+        public string BirthCertBase64 { get; set; }
+        public string TranscriptBase64 { get; set; }
 
         public NewApplicationRequestDTO() { }
     }
