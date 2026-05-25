c2fa2d0dd74c00fc75bc3158397435268276d98e
bilmoh287 <bilmoh287@gmail.com>
2026-05-25 04:31:29 -0800

    Created Forms for Application Review also adjusted the review student form to provide more documents

---
+++ DVLDPresentationLayer/Applications/Application Reviews/frmReviewRenewalApplications.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace DVLDPresentationLayer.Applications.Application_Reviews
+{
+    partial class frmReviewRenewalApplications
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDetail = new System.Windows.Forms.Panel();
+            this.btnReject = new Guna.UI2.WinForms.Guna2Button();
+            this.btnApprove = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctlPersonCard1 = new DVLDPresentationLayer.ctlPersonCard();
+            this.dgvUnderReviewList = new System.Windows.Forms.DataGridView();
+            this.pbIdpictrue = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelDetail.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUnderReviewList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIdpictrue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelDetail
+            // 
+            this.panelDetail.AutoScroll = true;
+            this.panelDetail.Controls.Add(this.btnReject);
+            this.panelDetail.Controls.Add(this.btnApprove);
+            this.panelDetail.Controls.Add(this.label1);
+            this.panelDetail.Controls.Add(this.pbIdpictrue);
+            this.panelDetail.Controls.Add(this.ctlPersonCard1);
+            this.panelDetail.Location = new System.Drawing.Point(15, 61);
+            this.panelDetail.Name = "panelDetail";
+            this.panelDetail.Size = new System.Drawing.Size(998, 298);
+            this.panelDetail.TabIndex = 6;
+            this.panelDetail.Visible = false;
+            // 
+            // btnReject
+            // 
+            this.btnReject.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnReject.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnReject.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnReject.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnReject.FillColor = System.Drawing.Color.Crimson;
+            this.btnReject.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnReject.ForeColor = System.Drawing.Color.White;
+            this.btnReject.Location = new System.Drawing.Point(861, 117);
+            this.btnReject.Name = "btnReject";
+            this.btnReject.Size = new System.Drawing.Size(111, 37);
+            this.btnReject.TabIndex = 4;
+            this.btnReject.Text = "Reject";
+            // 
+            // btnApprove
+            // 
+            this.btnApprove.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnApprove.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnApprove.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnApprove.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnApprove.FillColor = System.Drawing.Color.LimeGreen;
+            this.btnApprove.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnApprove.ForeColor = System.Drawing.Color.White;
+            this.btnApprove.Location = new System.Drawing.Point(861, 59);
+            this.btnApprove.Name = "btnApprove";
+            this.btnApprove.Size = new System.Drawing.Size(111, 37);
+            this.btnApprove.TabIndex = 3;
+            this.btnApprove.Text = "Approve";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vision health Medical Proof:";
+            // 
+            // ctlPersonCard1
+            // 
+            this.ctlPersonCard1.BackColor = System.Drawing.Color.White;
+            this.ctlPersonCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctlPersonCard1.Location = new System.Drawing.Point(13, 10);
+            this.ctlPersonCard1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ctlPersonCard1.Name = "ctlPersonCard1";
+            this.ctlPersonCard1.Size = new System.Drawing.Size(841, 285);
+            this.ctlPersonCard1.TabIndex = 0;
+            // 
+            // dgvUnderReviewList
+            // 
+            this.dgvUnderReviewList.AllowUserToAddRows = false;
+            this.dgvUnderReviewList.AllowUserToDeleteRows = false;
+            this.dgvUnderReviewList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvUnderReviewList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUnderReviewList.Location = new System.Drawing.Point(14, 387);
+            this.dgvUnderReviewList.Name = "dgvUnderReviewList";
+            this.dgvUnderReviewList.ReadOnly = true;
+            this.dgvUnderReviewList.RowHeadersWidth = 51;
+            this.dgvUnderReviewList.RowTemplate.Height = 24;
+            this.dgvUnderReviewList.Size = new System.Drawing.Size(983, 283);
+            this.dgvUnderReviewList.TabIndex = 5;
+            // 
+            // pbIdpictrue
+            // 
+            this.pbIdpictrue.Location = new System.Drawing.Point(303, 303);
+            this.pbIdpictrue.Name = "pbIdpictrue";
+            this.pbIdpictrue.Size = new System.Drawing.Size(445, 411);
+            this.pbIdpictrue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbIdpictrue.TabIndex = 1;
+            this.pbIdpictrue.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(876, 677);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(120, 39);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTitle.Location = new System.Drawing.Point(175, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(588, 49);
+            this.lblTitle.TabIndex = 136;
+            this.lblTitle.Text = "Reviw Renewal Application\r\n";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmReviewRenewalApplications
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1020, 723);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.panelDetail);
+            this.Controls.Add(this.dgvUnderReviewList);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmReviewRenewalApplications";
+            this.Text = "frmReviewRenewalApplications";
+            this.panelDetail.ResumeLayout(false);
+            this.panelDetail.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUnderReviewList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIdpictrue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDetail;
+        private Guna.UI2.WinForms.Guna2Button btnReject;
+        private Guna.UI2.WinForms.Guna2Button btnApprove;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbIdpictrue;
+        private ctlPersonCard ctlPersonCard1;
+        private System.Windows.Forms.DataGridView dgvUnderReviewList;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
--- DVLDPresentationLayer/frmUnderReview.Designer.cs
+++ DVLDPresentationLayer/frmUnderReview.Designer.cs
@@ -33,12 +33,20 @@ namespace DVLDPresentationLayer
             this.btnReject = new Guna.UI2.WinForms.Guna2Button();
             this.btnApprove = new Guna.UI2.WinForms.Guna2Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.pbIdpictrue = new System.Windows.Forms.PictureBox();
             this.ctlPersonCard1 = new DVLDPresentationLayer.ctlPersonCard();
-            this.lblTitle = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbidpictureback = new System.Windows.Forms.PictureBox();
+            this.pbBirthCertificate = new System.Windows.Forms.PictureBox();
+            this.pbIdpictruefront = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pb12thTranscript = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUnderReviewList)).BeginInit();
             this.panelDetail.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbIdpictrue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbidpictureback)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBirthCertificate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIdpictruefront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12thTranscript)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvUnderReviewList
@@ -47,26 +55,31 @@ namespace DVLDPresentationLayer
             this.dgvUnderReviewList.AllowUserToDeleteRows = false;
             this.dgvUnderReviewList.BackgroundColor = System.Drawing.Color.White;
             this.dgvUnderReviewList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUnderReviewList.Location = new System.Drawing.Point(13, 415);
+            this.dgvUnderReviewList.Location = new System.Drawing.Point(12, 426);
             this.dgvUnderReviewList.Name = "dgvUnderReviewList";
             this.dgvUnderReviewList.ReadOnly = true;
             this.dgvUnderReviewList.RowHeadersWidth = 51;
             this.dgvUnderReviewList.RowTemplate.Height = 24;
-            this.dgvUnderReviewList.Size = new System.Drawing.Size(973, 320);
+            this.dgvUnderReviewList.Size = new System.Drawing.Size(973, 282);
             this.dgvUnderReviewList.TabIndex = 2;
             this.dgvUnderReviewList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUnderReviewList_CellContentClick_1);
             // 
             // panelDetail
             // 
             this.panelDetail.AutoScroll = true;
+            this.panelDetail.Controls.Add(this.pb12thTranscript);
+            this.panelDetail.Controls.Add(this.label3);
+            this.panelDetail.Controls.Add(this.label2);
+            this.panelDetail.Controls.Add(this.pbidpictureback);
+            this.panelDetail.Controls.Add(this.pbBirthCertificate);
             this.panelDetail.Controls.Add(this.btnReject);
             this.panelDetail.Controls.Add(this.btnApprove);
             this.panelDetail.Controls.Add(this.label1);
-            this.panelDetail.Controls.Add(this.pbIdpictrue);
+            this.panelDetail.Controls.Add(this.pbIdpictruefront);
             this.panelDetail.Controls.Add(this.ctlPersonCard1);
-            this.panelDetail.Location = new System.Drawing.Point(0, 72);
+            this.panelDetail.Location = new System.Drawing.Point(0, 0);
             this.panelDetail.Name = "panelDetail";
-            this.panelDetail.Size = new System.Drawing.Size(998, 318);
+            this.panelDetail.Size = new System.Drawing.Size(1000, 402);
             this.panelDetail.TabIndex = 3;
             this.panelDetail.Visible = false;
             this.panelDetail.Paint += new System.Windows.Forms.PaintEventHandler(this.panelDetail_Paint);
@@ -106,19 +119,11 @@ namespace DVLDPresentationLayer
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 374);
+            this.label1.Location = new System.Drawing.Point(12, 359);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.Size = new System.Drawing.Size(143, 25);
             this.label1.TabIndex = 2;
-            this.label1.Text = "ID picture:";
-            // 
-            // pbIdpictrue
-            // 
-            this.pbIdpictrue.Location = new System.Drawing.Point(141, 335);
-            this.pbIdpictrue.Name = "pbIdpictrue";
-            this.pbIdpictrue.Size = new System.Drawing.Size(421, 216);
-            this.pbIdpictrue.TabIndex = 1;
-            this.pbIdpictrue.TabStop = false;
+            this.label1.Text = "ID picture front:";
             // 
             // ctlPersonCard1
             // 
@@ -130,24 +135,72 @@ namespace DVLDPresentationLayer
             this.ctlPersonCard1.Size = new System.Drawing.Size(841, 285);
             this.ctlPersonCard1.TabIndex = 0;
             // 
-            // lblTitle
+            // label2
             // 
-            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.ForeColor = System.Drawing.Color.Red;
-            this.lblTitle.Location = new System.Drawing.Point(303, 23);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(339, 33);
-            this.lblTitle.TabIndex = 4;
-            this.lblTitle.Text = "Under Review Students List";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ID picture:";
+            // 
+            // pbidpictureback
+            // 
+            this.pbidpictureback.Location = new System.Drawing.Point(645, 293);
+            this.pbidpictureback.Name = "pbidpictureback";
+            this.pbidpictureback.Size = new System.Drawing.Size(327, 170);
+            this.pbidpictureback.TabIndex = 6;
+            this.pbidpictureback.TabStop = false;
+            // 
+            // pbBirthCertificate
+            // 
+            this.pbBirthCertificate.Location = new System.Drawing.Point(70, 479);
+            this.pbBirthCertificate.Name = "pbBirthCertificate";
+            this.pbBirthCertificate.Size = new System.Drawing.Size(421, 244);
+            this.pbBirthCertificate.TabIndex = 5;
+            this.pbBirthCertificate.TabStop = false;
+            // 
+            // pbIdpictruefront
+            // 
+            this.pbIdpictruefront.Location = new System.Drawing.Point(161, 293);
+            this.pbIdpictruefront.Name = "pbIdpictruefront";
+            this.pbIdpictruefront.Size = new System.Drawing.Size(330, 170);
+            this.pbIdpictruefront.TabIndex = 1;
+            this.pbIdpictruefront.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ID picture back:";
+            // 
+            // pb12thTranscript
+            // 
+            this.pb12thTranscript.Location = new System.Drawing.Point(551, 479);
+            this.pb12thTranscript.Name = "pb12thTranscript";
+            this.pb12thTranscript.Size = new System.Drawing.Size(421, 244);
+            this.pb12thTranscript.TabIndex = 9;
+            this.pb12thTranscript.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(861, 714);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(125, 38);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
             // frmUnderReview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(998, 735);
-            this.Controls.Add(this.lblTitle);
+            this.ClientSize = new System.Drawing.Size(998, 759);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.panelDetail);
             this.Controls.Add(this.dgvUnderReviewList);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -159,9 +212,11 @@ namespace DVLDPresentationLayer
             ((System.ComponentModel.ISupportInitialize)(this.dgvUnderReviewList)).EndInit();
             this.panelDetail.ResumeLayout(false);
             this.panelDetail.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbIdpictrue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbidpictureback)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBirthCertificate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIdpictruefront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12thTranscript)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -169,10 +224,15 @@ namespace DVLDPresentationLayer
         private System.Windows.Forms.DataGridView dgvUnderReviewList;
         private System.Windows.Forms.Panel panelDetail;
         private ctlPersonCard ctlPersonCard1;
-        private System.Windows.Forms.PictureBox pbIdpictrue;
+        private System.Windows.Forms.PictureBox pbIdpictruefront;
         private Guna.UI2.WinForms.Guna2Button btnReject;
         private Guna.UI2.WinForms.Guna2Button btnApprove;
         private System.Windows.Forms.Label label1;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbidpictureback;
+        private System.Windows.Forms.PictureBox pbBirthCertificate;
+        private System.Windows.Forms.PictureBox pb12thTranscript;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClose;
     }
 }
