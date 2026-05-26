b22d446e166ac02e917795f29c0a89e44bbe4007
bilmoh287 <bilmoh287@gmail.com>
2026-05-26 02:40:16 -0800

    feat: implement local driving license application and institute management controllers

--- DVLDREST_API/Controllers/LicensesController.cs
+++ DVLDREST_API/Controllers/LicensesController.cs
@@ -222,6 +222,28 @@ namespace DVLDREST_API.Controllers
         {
             if (string.IsNullOrEmpty(base64String)) return null;
 
+            if (base64String == "existing")
+            {
+                try
+                {
+                    string folderPath = @"C:\DVLD_Uploads\Applications";
+                    if (!System.IO.Directory.Exists(folderPath))
+                    {
+                        System.IO.Directory.CreateDirectory(folderPath);
+                    }
+                    string placeholderPath = System.IO.Path.Combine(folderPath, $"placeholder_{docType}.jpg");
+                    if (!System.IO.File.Exists(placeholderPath))
+                    {
+                        System.IO.File.WriteAllBytes(placeholderPath, new byte[] { 0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46 });
+                    }
+                    return placeholderPath;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
             try
             {
                 string folderPath = @"C:\DVLD_Uploads\Applications";
